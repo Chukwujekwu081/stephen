e1fcb5020d501ae64f49ea5a5c7532136094e751
༒☬ßté෴vëñ☬༒ <chukwujekwustephen081@gmail.com>
2025-05-22 22:59:10 +0100

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const menuicon = document.querySelector('#menu-icon');
-const navbar = document.querySelector('.navbar');
-
-menuicon.onclick = () => {
-    menuicon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
